19e2c7d069a5d56bafd9543e410e934dc675df77
Zachery Son <zacheryyson@gmail.com>
2020-07-12 13:01:12 +0900

    Updates

---
+++ precache-manifest.d308296eb7814f5a1a82fbb9e85eef10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b08c12bf60a66f2c24cabcb41808cc64",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "885203f65ea32c13d1d5",
+    "url": "/movie_app/static/css/main.0aace6e0.chunk.css"
+  },
+  {
+    "revision": "c0071c60e1a78cbbff2e",
+    "url": "/movie_app/static/js/2.ce898f91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.ce898f91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "885203f65ea32c13d1d5",
+    "url": "/movie_app/static/js/main.d6dc1406.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
